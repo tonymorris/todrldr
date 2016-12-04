cfaff571cd5ace0fcaff9b2ef43bd15561e06954
Joshua Morris <joshuajohnmorris@gmail.com>
2016-12-04 18:21:35 +1000

    diagram with plots

--- MomentEnvelope.hs
+++ MomentEnvelope.hs
@@ -7,7 +7,7 @@ import Diagrams.Backend.Rasterific.CmdLine
 import Data.Typeable
 
 _polygon1 = [(120.5, 2550), (71, 1500), (52.5,1500), (68,1950), (104.5, 2550)]
-_polygon2 = [(61, 1500), (89, 2200), (82.5, 2200)]
+_polygon2 = [(61, 1500), (89, 2200), (83, 2200)]
 
 
 myaxis :: Axis B V2 Double
